7de813507a0bb8b1be492a02c9d8fe22600ce114
Andreev Dmitry <91731413+Pro100Dimka@users.noreply.github.com>
2023-09-15 19:40:56 +0300

    dr

--- routes/type.js
+++ routes/type.js
@@ -4,7 +4,7 @@ const router = new Router();
 const checkRole = require('../middleware/checkRole');
 
 // запросы на добавление и получения данных
-router.post('/', checkRole('ADMIN'), typeController.create);
+router.post('/create', checkRole('ADMIN'), typeController.create);
 router.post('/update/:id', checkRole('ADMIN'), typeController.updateItemById);
 router.get('/', typeController.getAllItems);
 router.get('/:id', typeController.getItemById);
